e0b20abd50fcf42843d0c085566c54249624db42
Murilo Gustineli <murilogustineli@gmail.com>
2026-01-21 10:38:35 -0500

    updated wordle 477-215-474, word=CUBIC

--- wordle/cli.py
+++ wordle/cli.py
@@ -8,11 +8,17 @@ from wordle.utils import Wordle
 def main():
     parser = argparse.ArgumentParser(description="Wordle solver and score tracker")
     parser.add_argument("--score", action="store_true", help="Display current scores")
+    parser.add_argument("--update", action="store_true", help="Update score after a game")
     args = parser.parse_args()
 
     if args.score:
         wordle = Wordle()
         df = wordle.load_data()
+        print(tabulate(df, headers="keys", tablefmt="rounded_grid", showindex=False))
+    elif args.update:
+        wordle = Wordle()
+        df = wordle.update_score()
+        print("\nUpdated scores:")
         print(tabulate(df, headers="keys", tablefmt="rounded_grid", showindex=False))
     else:
         parser.print_help()
